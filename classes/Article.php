410136753334224741252f61e0e0fbcd9c574b5b
Antoan Petrov <antoan999_kn@abv.bg>
2022-12-28 18:27:30 +0200

    create Article class and get all articles method

---
+++ classes/Article.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Article
+ * 
+ * A piece of writing for publishion
+ */
+class Article
+{
+    /**
+     * Get all articles
+     * @param object $conn Connection to the database
+     * 
+     * @return array An associative array of all the article records
+     */
+    public static function getAll($conn)
+    {
+        $sql = "SELECT *
+        FROM article
+        ORDER BY published_at;";
+
+        $results = $conn->query($sql);
+
+        return $results->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
