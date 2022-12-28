66ae5538ff4dcaf45de5562fc12b36cfb5a62117
Antoan Petrov <antoan999_kn@abv.bg>
2022-12-28 19:34:42 +0200

    move validation method to Article class

--- classes/Article.php
+++ classes/Article.php
@@ -15,11 +15,15 @@ class Article
      * @var string
      * Published date and time
      * @var datetime
+     * * Validation errors
+     * @var array
      */
     public $id;
     public $title;
     public $content;
     public $published_at;
+    public $errors = [];
+
 
     /**
      * Get all articles
@@ -72,22 +76,86 @@ class Article
      */
     public function update($conn)
     {
-        $sql = "UPDATE article
+        if ($this->validate()) {
+
+            $sql = "UPDATE article
         SET title = :title,
             content = :content,
             published_at = :published_at
         WHERE id = :id";
 
-        $stmt = $conn->prepare($sql);
-        $stmt->bindValue(':id', $this->id, PDO::PARAM_INT);
-        $stmt->bindValue(':title', $this->title, PDO::PARAM_STR);
-        $stmt->bindValue(':content', $this->content, PDO::PARAM_STR);
+            $stmt = $conn->prepare($sql);
+            $stmt->bindValue(':id', $this->id, PDO::PARAM_INT);
+            $stmt->bindValue(':title', $this->title, PDO::PARAM_STR);
+            $stmt->bindValue(':content', $this->content, PDO::PARAM_STR);
 
-        if ($this->published_at == '') {
-            $stmt->bindValue(':published_at', null, PDO::PARAM_NULL);
+            if ($this->published_at == '') {
+                $stmt->bindValue(':published_at', null, PDO::PARAM_NULL);
+            } else {
+                $stmt->bindValue(':published_at', $this->published_at, PDO::PARAM_STR);
+            }
+            return $stmt->execute();
         } else {
-            $stmt->bindValue(':published_at', $this->published_at, PDO::PARAM_STR);
+            return false;
         }
-        return $stmt->execute();
+    }
+
+    /**
+     * Get the article record based on the ID
+     *
+     * @param object $conn Connection to the database
+     * @param integer $id the article ID
+     * @param string $columns Optional list of columns for the select, defaults to *
+     *
+     * @return mixed An associative array containing the article with that ID, or null if not found
+     */
+    function getArticle($conn, $id, $columns = '*')
+    {
+        $sql = "SELECT $columns
+            FROM article
+            WHERE id = :id";
+
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+
+        if ($stmt->execute()) {
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        }
+    }
+
+    /**
+     * Validate the article properties
+     *
+     * @return boolean True if current properties are valid, false otherwise
+     */
+    protected function validate()
+    {
+
+        if ($this->title == '') {
+            $this->errors[] = 'Title is required';
+        }
+        if ($this->content == '') {
+            $this->errors[] = 'Content is required';
+        }
+
+        if ($this->published_at != '') {
+            $date_time = date_create_from_format('Y-m-d H:i:s', $this->published_at);
+
+            if ($date_time === false) {
+
+                $this->errors[] = 'Invalid date and time';
+
+            } else {
+
+                $date_errors = date_get_last_errors();
+
+                if ($date_errors['warning_count'] > 0) {
+                    $this->errors[] = 'Invalid date and time';
+                }
+            }
+        }
+
+        return empty($this->errors);
     }
 }
