0aad1f764ede589e9e8790e7f496dda5b54be7c5
Antoan Petrov <antoan999_kn@abv.bg>
2022-12-29 11:13:18 +0200

    compare password and hash password

--- classes/User.php
+++ classes/User.php
@@ -37,7 +37,7 @@ class User
 
 
         if ($user = $stmt->fetch()) {
-            return $user->password == $password;
+            return password_verify($password, $user->password);
         }
     }
 }
